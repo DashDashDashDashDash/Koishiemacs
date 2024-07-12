4aa81b166df074f3e95830b9d269c0ac57f09171
Dash- <dash@dashda.sh>
2024-07-11 22:40:22 -0300

    company min prefix length 2...

--- init.el
+++ init.el
@@ -298,7 +298,7 @@
     company-abort
   :config
   (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 1)
+  (setq company-minimum-prefix-length 2)
   (define-key company-active-map (kbd "M-n") nil)
   (define-key company-active-map (kbd "M-p") nil)
   (define-key company-active-map (kbd "C-n") #'company-select-next)
