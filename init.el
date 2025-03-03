1b533738c750f80ef55f94d0ad7e4d6d2699f71a
Dash- <dash@dashda.sh>
2025-03-03 01:16:37 -0300

    replace ivy+counsel+swiper with vertico...

--- init.el
+++ init.el
@@ -111,16 +111,45 @@
 (setq use-package-always-ensure t
       use-package-expand-minimally t)
 
-(use-package ivy
+;; Enable Vertico.
+(use-package vertico
+  :custom
+  ;; (vertico-scroll-margin 0) ;; Different scroll margin
+  ;; (vertico-count 20) ;; Show more candidates
+  ;; (vertico-resize t) ;; Grow and shrink the Vertico minibuffer
+  ;; (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
   :init
-  (ivy-mode t))
+  (vertico-mode))
 
-(use-package counsel
-  :config
-  (counsel-mode t))
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
 
-(use-package swiper
-  :bind ("C-s" . 'swiper))
+;; Emacs minibuffer configurations.
+(use-package emacs
+  :custom
+  ;; Support opening new minibuffers from inside existing minibuffers.
+  (enable-recursive-minibuffers t)
+  ;; Hide commands in M-x which do not work in the current mode.  Vertico
+  ;; commands are hidden in normal buffers. This setting is useful beyond
+  ;; Vertico.
+  (read-extended-command-predicate #'command-completion-default-include-p)
+  ;; Do not allow the cursor in the minibuffer prompt
+  (minibuffer-prompt-properties
+   '(read-only t cursor-intangible t face minibuffer-prompt)))
+;; "I recommend to give Orderless completion a try, which is more flexible and powerful than the default completion styles."
+
+;; Optionally use the `orderless' completion style.
+(use-package orderless
+  :custom
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch))
+  ;; (orderless-component-separator #'orderless-escapable-split-on-space)
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides '((file (styles partial-completion)))))
+
 
 (use-package doom-themes
   :ensure t
