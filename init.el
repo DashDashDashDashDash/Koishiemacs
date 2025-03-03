c0e25b3ddbee0c2eefba7acc9cdcda7aba1e7faa
Dash- <dash@dashda.sh>
2025-03-03 16:31:08 -0300

    stop checking for syntax errors on elisp files(?)

--- init.el
+++ init.el
@@ -614,7 +614,8 @@
   (setq flycheck-indication-mode 'right-fringe)
   (define-fringe-bitmap 'flycheck-fringe-bitmap-double-arrow
       [16 48 112 240 112 48 16] nil nil 'center)
-  (global-flycheck-mode))
+  (global-flycheck-mode)
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
 
 (use-package beacon
   :init
