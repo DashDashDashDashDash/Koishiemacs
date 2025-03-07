de0c995c966c8d398f5c524a53bd336cd78166fb
Dash- <dash@dashda.sh>
2025-03-06 23:23:34 -0300

    redirect js-mode to js-ts-mode

--- init.el
+++ init.el
@@ -167,6 +167,7 @@
  '((yaml-mode . yaml-ts-mode)
    (bash-mode . bash-ts-mode)
    (js2-mode . js-ts-mode)
+   (js-mode . js-ts-mode)
    (typescript-mode . typescript-ts-mode)
    (json-mode . json-ts-mode)
    (css-mode . css-ts-mode)
