0c7cb9e2d6137a227fd8e7772a2aa038f25f4124
Dash- <dash@dashda.sh>
2025-04-17 20:30:16 -0300

    turn on git-gutter-mode on text-mode buffers only

--- init.el
+++ init.el
@@ -333,14 +333,15 @@
 
 (use-package git-gutter-fringe
   :config
-  (global-git-gutter-mode)
   (setq git-gutter:update-interval 0.2)
   (define-fringe-bitmap 'git-gutter-fr:added [224]
       nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:modified [224]
       nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240]
-      nil nil 'bottom))
+    nil nil 'bottom)
+  :hook
+  (text-mode . git-gutter-mode))
 
 (use-package avy
   :bind ("C-;" . 'avy-goto-char)
